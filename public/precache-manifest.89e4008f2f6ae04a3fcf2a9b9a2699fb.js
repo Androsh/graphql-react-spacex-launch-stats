3450c27cbc38b87fadd35166e6c4cc758f755add
androsh <apanasiuk00@gmail.com>
2019-02-07 15:46:46 +0800

    Finished app, preparing for deployment

---
+++ public/precache-manifest.89e4008f2f6ae04a3fcf2a9b9a2699fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3765d6ccc09357692cc841812686a1d6",
+    "url": "/static/media/logo.3765d6cc.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2f4546192033138f9d47",
+    "url": "/static/js/main.2f454619.chunk.js"
+  },
+  {
+    "revision": "97224eee8177b29d0f20",
+    "url": "/static/js/1.97224eee.chunk.js"
+  },
+  {
+    "revision": "2f4546192033138f9d47",
+    "url": "/static/css/main.fa7177d0.chunk.css"
+  },
+  {
+    "revision": "60961ab10a0bdf969743d8260768f331",
+    "url": "/index.html"
+  }
+];
